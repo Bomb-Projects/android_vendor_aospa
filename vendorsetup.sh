9e78aaf502f2628b462cf2305bd672331183355a
L1ghtzin <lightzin021@gmail.com>
2025-04-11 19:51:56 -0300

    a

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-# Shebang is intentionally missing - do not run as a script
+ada# Shebang is intentionally missing - do not run as a script
 
 # Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_USERNAME=nobody
